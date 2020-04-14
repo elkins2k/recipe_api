1da05165ce4318eaaa344180161f6b0ad43a08c2
Jeff Elkins <elkins2k@mail.com>
2020-04-14 15:09:41 -0500

    mongoURI

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require ( 'mongoose' )
 
-let mongoURI = 'mongodb://localhost/recipe-api'
+// let mongoURI = 'mongodb://localhost/recipe-api'
+let mongoURI = 'mongodb://heroku_9g1pmgbz:28uami2rardl2ctpnt1ohcj64e@ds241737.mlab.com:41737/heroku_9g1pmgbz'
 
 if (process.env.NODE_ENV === 'production') {
     mongoURI = process.env.MONGODB_URI
