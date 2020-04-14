e0d0aff76a324dbcdfb5e91b0c71bbca4057de84
Jeff Elkins <elkins2k@mail.com>
2020-04-14 14:46:56 -0500

    added cors

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// const cors = require('cors')
-// app.use(cors())
+const cors = require('cors')
+app.use(cors())
 
 app.use('/api/users', require('./controllers/users'))
 app.use('/api/recipes', require('./controllers/recipes'))
