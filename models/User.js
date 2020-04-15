b033f0df9a7cbaec68bdff3d4788a09b74e8c2e7
Jeff Elkins <elkins2k@mail.com>
2020-04-15 18:39:11 -0500

    added user preference tags

--- models/User.js
+++ models/User.js
@@ -3,7 +3,18 @@ const mongoose = require ( '../db/connection' )
 const User = new mongoose.Schema ({
   email: String,
   firstName: String,
-  lastName: String
+  lastName: String,
+  avoids: {
+    carbs: Boolean,
+    dairy: Boolean,
+    eggs: Boolean,
+    gluten: Boolean,
+    meat: Boolean,
+    nuts: Boolean,
+    quinoa: Boolean,
+    shellfish: Boolean,
+    spicy: Boolean    
+  }
 })
 
 module.exports = mongoose.model ( 'User', User )
