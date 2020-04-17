02fe510cb9bdcb024f650feb62cf80b2e85154b8
Jeff Elkins <elkins2k@mail.com>
2020-04-17 08:26:01 -0500

    added whitelist for CORS

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-var whitelist = ['http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
-// app.use(cors())
+// var whitelist = ['http://localhost:3000', "*"]
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
+app.use(cors())
 
 app.use('/api/users', require('./controllers/users'))
 app.use('/api/recipes', require('./controllers/recipes'))
