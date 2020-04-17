ce5faa5c246cddb934e018fcd11c639ab35174fe
Jeff Elkins <elkins2k@mail.com>
2020-04-17 08:33:15 -0500

    Access-Control-Allow_Origin res header

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 // var whitelist = ['http://localhost:3000', "*"]
 // var corsOptions = {
 //   origin: function (origin, callback) {
